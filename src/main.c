5f89918ba22e49f9971ef78e8726d2727900f964
HomekitItalia <lizzus@me.com>
2019-05-05 13:38:04 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,26 +1,3 @@
-/*
- * Example of using esp-homekit library to control
- * a simple $5 Sonoff Basic using HomeKit.
- * The esp-wifi-config library is also used in this
- * example. This means you don't have to specify
- * your network's SSID and password before building.
- *
- * In order to flash the sonoff basic you will have to
- * have a 3,3v (logic level) FTDI adapter.
- *
- * To flash this example connect 3,3v, TX, RX, GND
- * in this order, beginning in the (square) pin header
- * next to the button.
- * Next hold down the button and connect the FTDI adapter
- * to your computer. The sonoff is now in flash mode and
- * you can flash the custom firmware.
- *
- * WARNING: Do not connect the sonoff to AC while it's
- * connected to the FTDI adapter! This may fry your
- * computer and sonoff.
- *
- */
-
 #define DEVICE_MANUFACTURER "lizzus"
 #define DEVICE_NAME "ESP"
 #define DEVICE_MODEL "ESP01S"
